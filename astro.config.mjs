17ca22d1a2b3332841af5db68edb35e2d828b42f
Juan E. Gil <juane.gil2002@gmail.com>
2024-08-12 13:47:39 -0500

    feat: :sparkles: add login page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  server: {
+    host: process.env.HOST || "localhost",
+    port: process.env.PORT || 4321,
+  },
   integrations: [tailwind(), react()]
 });
--- src/components/dashboard/CustomersList.tsx
+++ src/components/dashboard/CustomersList.tsx
@@ -6,9 +6,9 @@ interface Client {
 
 const ClientList: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center max-w-md mx-auto">
       <h2 className="text-2xl font-bold mb-8 mt-4">Lista de Clientes</h2>
-      <ul className="list-disc pl-5">
+      <ul className="list-disc pl-5 max-w-md mx-auto">
         {data.map((client: Client) => (
           <li key={client.id} className="text-lg font-semibold mb-4">
             <span>{client.id}</span> -{" "}
