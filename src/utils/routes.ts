2a046ec8b7bd902db4d53197d7f537a36464f5fb
Juan E. Gil <juane.gil2002@gmail.com>
2024-08-12 15:05:29 -0500

    .

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -7,7 +7,7 @@ export function dbLogin(email: string, password: string): Promise<dbLoginType> {
   let username: string | null = null;
   let success = true;
   return new Promise((resolve, reject) => {
-    if (email === "user@example.com" && password === "1234") {
+    if (email === "jhondoe@example.com" && password === "1234ABC") {
       username = "User Test";
     } else {
       success = false;
