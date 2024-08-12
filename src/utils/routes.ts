f7657f906519cf4f048dcaf22c301a41a2b1264b
Juan E. Gil <juane.gil2002@gmail.com>
2024-08-12 15:57:37 -0500

    fix: :bug: fix login form

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -7,7 +7,7 @@ export function dbLogin(email: string, password: string): Promise<dbLoginType> {
   let username: string | null = null;
   let success = true;
   return new Promise((resolve, reject) => {
-    if (email === "jhondoe@example.com" && password === "1234ABC") {
+    if (email === "jhondoe@example.com" && password === "1234abc") {
       username = "User Test";
     } else {
       success = false;
